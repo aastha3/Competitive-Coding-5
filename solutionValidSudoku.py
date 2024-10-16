8adec05dabdd61b4e0df4b6747458ee847cfc876
dandelion <duaaastha3@gmail.com>
2024-10-16 00:33:49 -0700

    Create solutionValidSudoku.py
    Time complexity : size of chess board 
    Space complexity ;size of chess board 
    Passed all cases on LC : yes

---
+++ solutionValidSudoku.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        N = 9 
+
+        rows = [[] for _ in range(N)]
+        cols = [[] for _ in range(N)]
+        boxes = [[ [] for _ in range(3)] for _ in range(3)]
+
+
+        for r in range(N):
+            for c in range(N):
+                val = board[r][c]
+
+                # check if the position is filled with numeral or empty 
+                if val =='.':
+                    continue
+
+                idx_i = (r//3); idx_j =c//3 
+                # check the row
+                if val in rows[r] or val in cols[c] or val in boxes[idx_i][idx_j]:
+                    return False 
+                rows[r].append(val)
+
+                cols[c].append(val)
+                
+                boxes[idx_i][idx_j].append(val)
+        return True 
